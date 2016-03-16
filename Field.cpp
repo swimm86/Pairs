5c2b37289c247dbe4ade713249a7f2b57d9ca12b
Alexandr Akulich <akulichalexander@gmail.com>
2016-03-16 23:49:09 +0500

    Field: Added width and height properties.

--- Field.cpp
+++ Field.cpp
@@ -1,7 +1,29 @@
 #include "Field.hpp"
 
 Field::Field(QObject *parent) :
-    QObject(parent)
+    QObject(parent),
+    m_width(10),
+    m_height(10)
 {
 
+}
+
+void Field::setWidth(int width)
+{
+    if (m_width == width) {
+        return;
+    }
+
+    m_width = width;
+    emit widthChanged(width);
+}
+
+void Field::setHeight(int height)
+{
+    if (m_height == height) {
+        return;
+    }
+
+    m_height = height;
+    emit heightChanged(height);
 }
--- Field.hpp
+++ Field.hpp
@@ -6,12 +6,26 @@
 class Field : public QObject
 {
     Q_OBJECT
+    Q_PROPERTY(int width READ width WRITE setWidth NOTIFY widthChanged)
+    Q_PROPERTY(int height READ height WRITE setHeight NOTIFY heightChanged)
 public:
     explicit Field(QObject *parent = 0);
 
+    int width() const { return m_width; }
+    int height() const { return m_height; }
+
 signals:
+    void widthChanged(int width);
+    void heightChanged(int height);
 
 public slots:
+    void setWidth(int width);
+    void setHeight(int height);
+
+private:
+    int m_width;
+    int m_height;
+
 };
 
 #endif // FIELD_HPP
