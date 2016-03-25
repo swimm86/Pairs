26e8e9849641019959546f3fc2571352deee8027
Alexandr Akulich <akulichalexander@gmail.com>
2016-03-26 01:52:31 +0500

    main.cpp: Game logic classes registered in QML.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,17 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 
+#include <QtQml>
+
+#include "Cell.hpp"
+#include "Field.hpp"
+
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
+
+    qmlRegisterType<Cell>("Pairs", 1, 0, "Cell");
+    qmlRegisterType<Field>("Pairs", 1, 0, "Field");
 
     QQmlApplicationEngine engine;
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
